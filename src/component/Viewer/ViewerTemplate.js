9efbf3a9718e19aa1fcb89cd8dfc773558829dc0
juno <juno@localhost.localdomain>
2019-10-15 03:02:49 -0500

    update

--- src/component/Viewer/ViewerTemplate.js
+++ src/component/Viewer/ViewerTemplate.js
@@ -1,21 +1,18 @@
-import React from 'react';
-import styled from 'styled-components';
-import { Player } from 'video-react';
+import React from "react";
+import styled from "styled-components";
+import { Player } from "video-react";
 
 const w = e(window.innerWidth - 350);
 const h = window.innerHeight;
 function e(c) {
   const b = (c * 9) / 16;
   if (window.innerHeight - 100 < b) {
-    return ((window.innerHeight - 200) * 16) / 9;
+    return ((window.innerHeight - 150) * 16) / 9;
   } else {
     console.log(window.innerHeight);
     return c;
   }
 }
-const cal = e => {
-  return (e / 16) * 9;
-};
 
 const Square = styled.div`
   background: black;
