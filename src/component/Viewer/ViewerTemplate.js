7cc8b8026ec07e1fc535d060478d3c47b9129a2c
juno <juno@localhost.localdomain>
2019-10-17 01:30:37 -0500

    update

--- src/component/Viewer/ViewerTemplate.js
+++ src/component/Viewer/ViewerTemplate.js
@@ -1,27 +1,33 @@
 import React, { Component } from "react";
 import styled from "styled-components";
 import { Player } from "video-react";
+import "./ViewerTemplate.scss";
+const initSize = sizeCheck(window.innerWidth - 350);
 
-const w = e(window.innerWidth - 350);
-const h = window.innerHeight;
-function e(c) {
-  const b = (c * 9) / 16;
-  if (window.innerHeight - 100 < b) {
-    return ((window.innerHeight - 150) * 16) / 9;
+function sizeCheck(adaptedWidth) {
+  const height = (adaptedWidth * 9) / 16;
+  const adaptedHeight = window.innerHeight - 150;
+  if (adaptedWidth < 640) {
+    return 640;
+  } else if (adaptedHeight < height) {
+    console.log(adaptedHeight);
+    adaptedWidth = (adaptedHeight * 16) / 9;
+    if (adaptedHeight < 360) {
+      return 640;
+    }
+    return adaptedWidth;
   } else {
-    console.log(window.innerHeight);
-    return c;
+    return adaptedWidth;
   }
 }
 
 const Square = styled.div`
-  background: black;
   justify-content: center;
   margin: 1.5rem;
 `;
 
 class ViewerTemplate extends Component {
-  state = { width: w, height: w * (9 / 16) };
+  state = { width: initSize };
   componentDidMount() {
     window.addEventListener("resize", this.resize);
   }
@@ -30,16 +36,22 @@ class ViewerTemplate extends Component {
   }
   resize = () => {
     this.setState({
-      width: window.innerWidth - 350,
-      height: window.innerHeight
+      width: sizeCheck(window.innerWidth - 350)
     });
   };
   render() {
     console.log("sdddd" + this.state.width);
     return (
-      <Square
-        style={{ height: this.state.width * (9 / 16), width: this.state.width }}
-      />
+      <Square>
+        <Player
+          playsInline
+          height={this.state.width * (9 / 16)}
+          width={this.state.width}
+          fluid={false}
+          src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
+          type="video/mp4"
+        />
+      </Square>
     );
   }
 }
